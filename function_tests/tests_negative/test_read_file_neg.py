7fe4dfbb6b65cf1b7b78ce09854c24767be241ad
AlexKysil <alex.kysil0504@gmail.com>
2021-04-25 00:04:16 +0300

    added logger to tests

--- function_tests/tests_negative/test_read_file_neg.py
+++ function_tests/tests_negative/test_read_file_neg.py
@@ -15,10 +15,13 @@ LOGGER = logging.getLogger('logger')
 @pytest.mark.parametrize("path, expected_error", INVALID_FILE_PATHS)
 def test_read_by_wrong_path(path, expected_error):
 
+    LOGGER.info(f"Start test[negative]: test_read_by_wrong_path with path value: {path}")
     try:
+        LOGGER.info("Try to execute function")
         with ReadTeminalOutput():
             read_file_return_lines(path)
     except(TypeError, ValueError, FileNotFoundError) as ex:
+        LOGGER.info("Check error message")
         assert str(ex) == expected_error, \
             f'Actual Error for expected wrong File Path: {expected_error}, didn`t match: {ex}'
 
@@ -27,12 +30,15 @@ def test_read_by_wrong_path(path, expected_error):
 @pytest.mark.parametrize("valid_path, n_lines, expected_error", INVALID_NLINES)
 def test_read_with_wrong_nlines(valid_path, n_lines, expected_error):
 
+    LOGGER.info(f"Start test[negative]: test_read_with_wrong_nlines with n_lines value: {n_lines}")
     valid_path = construct_absolute_path(valid_path)
 
     try:
+        LOGGER.info("Try to execute function")
         with ReadTeminalOutput():
             read_file_return_lines(valid_path, n_lines)
     except(TypeError, FileNotFoundError) as ex:
+        LOGGER.info("Check error message")
         assert str(ex) == expected_error, \
             f"Error for wrong N_lines value didn`t match to expected: {expected_error}, actual value: {ex}"
 
@@ -40,11 +46,14 @@ def test_read_with_wrong_nlines(valid_path, n_lines, expected_error):
 @pytest.mark.negative
 def test_read_not_txt():
 
+    LOGGER.info("Start test[negative] : test_read_not_txt")
     path = construct_absolute_path(JSON_FILE)
 
     try:
         with ReadTeminalOutput():
+            LOGGER.info("Try to execute function")
             read_file_return_lines(path)
     except(ValueError) as ex:
+        LOGGER.info("Check error message")
         assert str(ex) == EXPECTED_JSON_ERROR, \
             f'Received error didn`t match to expected Wrong Format: {EXPECTED_JSON_ERROR}. Exception: {ex}.'
